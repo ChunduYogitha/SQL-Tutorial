909bacfab492e1484b658f6439dfd1b3710421fb
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-03-06 15:16:03 +0530

    01.sql intro.sql

--- 01.sql intro.sql
+++ 01.sql intro.sql
@@ -34,3 +34,14 @@ Look at the "Customers" table:
 Syntax
 SELECT * FROM Customers;
 
+Every table is broken up into smaller entities called fields. 
+The fields in the Customers table consist of CustomerID, CustomerName, ContactName, Address, City, PostalCode and Country. 
+A field is a column in a table that is designed to maintain specific information about every record in the table.
+
+A record, also called a row, is each individual entry that exists in a table. 
+For example, there are 91 records in the above Customers table. A record is a horizontal entity in a table.
+
+A column is a vertical entity in a table that contains all information associated with a specific field in a table.
+
+
+
