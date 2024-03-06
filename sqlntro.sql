7800f4fae5f41465d85f4899814e8ca467bd7da5
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-03-06 15:11:25 +0530

    sql intro.sql

---
+++ sqlntro.sql
@@ -0,0 +1,36 @@
+SQL is a standard language for storing, manipulating and retrieving data in databases.
+
+Will learn how to use SQL in: MySQL, SQL Server, MS Access, Oracle, Sybase, Informix, Postgres, and other database systems.
+
+What is SQL?
+SQL stands for Structured Query Language
+SQL lets you access and manipulate databases
+
+What Can SQL do?
+SQL can execute queries against a database
+SQL can retrieve data from a database
+SQL can insert records in a database
+SQL can update records in a database
+SQL can delete records from a database
+SQL can create new databases
+SQL can create new tables in a database
+SQL can create stored procedures in a database
+SQL can create views in a database
+SQL can set permissions on tables, procedures, and views
+
+Using SQL in Your Web Site
+To build a web site that shows data from a database, you will need:
+An RDBMS database program (i.e. MS Access, SQL Server, MySQL)
+To use a server-side scripting language, like PHP or ASP
+To use SQL to get the data you want
+To use HTML / CSS to style the page
+
+RDBMS
+RDBMS stands for Relational Database Management System.
+RDBMS is the basis for SQL, and for all modern database systems such as MS SQL Server, IBM DB2, Oracle, MySQL, and Microsoft Access.
+The data in RDBMS is stored in database objects called tables. A table is a collection of related data entries and it consists of columns and rows.
+
+Look at the "Customers" table:
+Syntax
+SELECT * FROM Customers;
+
