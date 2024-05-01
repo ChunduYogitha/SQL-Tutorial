651aaf37e30215c100bc3722a66cc8570359f774
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-05-01 22:52:15 +0530

    04.where clause.sql

--- 04.where clause.sql
+++ 04.where clause.sql
@@ -27,3 +27,42 @@ Select all customers with a CustomerID greater than 80:
 
 SELECT * FROM Customers
 WHERE CustomerID > 80;
+
+The following operators can be used in the WHERE clause:
+
+Example : Equal (=)
+SELECT * FROM Products
+WHERE Price = 18;
+
+Example : Less Than (<)
+SELECT * FROM Products
+WHERE Price = 18;
+
+Example : Greater than or equal ( >= )
+SELECT * FROM Products
+WHERE Price >= 30;
+
+Example : Less than or equal ( <= )
+SELECT * FROM Products
+WHERE Price <= 30;
+
+Example : Not equal (<> != )
+Not equal. Note: In some versions of SQL this operator may be written as !=
+SELECT * FROM Products
+WHERE Price <> 18;
+
+ Example : Between certain range (BETWEEN)
+SELECT * FROM Products
+WHERE Price BETWEEN 50 AND 60;
+
+Example : Search for a pattern
+SELECT * FROM Customers
+WHERE City LIKE 's%';
+
+Example : To specify multiple possible values for a column (IN)
+SELECT * FROM Customers
+WHERE City IN ('Paris','London');
+
+
+
+
