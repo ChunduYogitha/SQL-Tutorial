7f1549e7ba46b341f8da72d98c7ced8dc7997786
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-06-08 10:45:37 +0530

    09.INSERT INTO Operator.sql

--- 09.INSERT INTO Operator.sql
+++ 09.INSERT INTO Operator.sql
@@ -43,6 +43,16 @@ INSERT INTO Customers (CustomerName, City, Country)
 VALUES ('Cardinal', 'Stavanger', 'Norway');
 
 
+CustomerID	   CustomerName	                  ContactName	              Address	                              City	            PostalCode	                Country
+89	           White Clover Markets          	Karl Jablonski	          305 - 14th Ave. S. Suite              3B	Seattle	      98128                       	USA
+90             Wilman Kala	                  Matti Karttunen	           Keskuskatu 45	                      Helsinki	        21240	                      Finland
+91             Wolski	                        Zbyszek	                   ul. Filtrowa                         68	Walla	        01-012	                     Poland
+92	           Cardinal	                      null	                      null 	                              Stavanger         	null	                     Norway
+
+
+
+
+
 
 
 
