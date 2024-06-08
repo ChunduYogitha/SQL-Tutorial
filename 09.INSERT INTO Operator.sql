439d29996d03ea244a616768ce36332bc52c0c72
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-06-08 10:55:13 +0530

    09.INSERT INTO Operator.sql

--- 09.INSERT INTO Operator.sql
+++ 09.INSERT INTO Operator.sql
@@ -50,8 +50,29 @@ CustomerID	   CustomerName	                  ContactName	              Address	 
 92	           Cardinal	                      null	                      null 	                              Stavanger         	null	                     Norway
 
 
+Insert Multiple Rows
+It is also possible to insert multiple rows in one statement.
 
+To insert multiple rows of data, we use the same INSERT INTO statement, but with multiple values:
 
+Example
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES
+('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway'),
+('Greasy Burger', 'Per Olsen', 'Gateveien 15', 'Sandnes', '4306', 'Norway'),
+('Tasty Tee', 'Finn Egan', 'Streetroad 19B', 'Liverpool', 'L1 0AA', 'UK');
+
+Make sure you separate each set of values with a comma ,.
+
+The selection from the "Customers" table will now look like this:
+
+CustomerID	CustomerName	                ContactName	                    Address	                          City	              PostalCode	        Country
+89	        White Clover Markets	        Karl Jablonski	                305 - 14th Ave. S. Suite 3B	      Seattle	            98128	                USA
+90          Wilman Kala	                  Matti Karttunen	                Keskuskatu 45	                     Helsinki	          21240	              Finland
+91          Wolski	                      Zbyszek	                        ul. Filtrowa 68	                   Walla	            01-012	             Poland
+92	        Cardinal	                    Tom B. Erichsen	                Skagen 21	                         Stavanger	         4006	               Norway
+93	        Greasy Burger	                Per Olsen	                      Gateveien 15	                     Sandnes	           4306	               Norway
+94	        Tasty Tee	                    Finn Egan	                      Streetroad 19B	                   Liverpool	         L1 0AA              	UK
 
 
 
